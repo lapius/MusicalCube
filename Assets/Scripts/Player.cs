276a45b1d1e578b0b67e3cbbe6751dc3fc9de920
Marijus Lapinskas <marijus.lapinskas@gmail.com>
2017-01-21 01:39:10 +0200

    Player kodas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,35 @@ public class Player : MonoBehaviour {
 	public float jumpForce;
 	private Rigidbody2D rb;
 
+	public LayerMask ground;
+	private bool isGrounded;
+	private Collider2D col;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		col = GetComponent<Collider2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+		isGrounded = Physics2D.IsTouchingLayers (col, ground);
 
 		rb.AddForce (transform.right * speed);
-	
+		if (isGrounded) {
+			rb.velocity = new Vector2 (speed, 0);
+		}
+
+		if(Input.GetKey("space")){
+			if(isGrounded){
+				rb.AddForce (transform.up * jumpForce);
+			}
+		}
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+
 	}
 }
