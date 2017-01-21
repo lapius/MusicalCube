f42e86c1764d02d0f8f4b88efe053307578a1260
Marijus Lapinskas <marijus.lapinskas@gmail.com>
2017-01-21 04:30:15 +0200

    Veikiantis normalei player scriptas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,45 +6,58 @@ public class Player : MonoBehaviour {
 	public float speed;
 	public float jumpForce;
 	public float rotSpeed;
-
+	private Vector3 go;
+	public GameObject cameraa;
+	private Vector3 camPos;
+	private float dist;
 
 	private Rigidbody2D rb;
 
 	public LayerMask ground;
 	private bool isGrounded;
+	private bool isJumped;
 	private Collider2D col;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		col = GetComponent<Collider2D> ();
-		rb.AddForce (transform.up * jumpForce * 100);
+		isJumped = true;
+		rb.AddForce (transform.up * jumpForce*100);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		
+		dist = transform.position.x - camPos.x;
+		cameraa.transform.position = new Vector3 (cameraa.transform.position.x + dist, cameraa.transform.position.y, cameraa.transform.position.z);
+		camPos = cameraa.transform.position;
 
 		isGrounded = Physics2D.IsTouchingLayers (col, ground);
 
-		rb.velocity = new Vector2 (speed, rb.velocity.y);
+
 		if (isGrounded) {
+
 			rb.velocity = new Vector2 (speed, 0);
+			//transform.Translate (speed/10, 0, 0);
+
 			this.gameObject.transform.Rotate(0, 0, 0);
-			transform.rotation = Quaternion.Euler(0,0,0);
+			transform.localRotation  = Quaternion.Euler(0,0,0);
 		} else if (!isGrounded) {
-			this.gameObject.transform.Rotate (0, 0, Time.deltaTime * rotSpeed);
+			if (isJumped) {
+				this.gameObject.transform.Rotate (0, 0, Time.deltaTime * rotSpeed);
+			}
+			rb.velocity = new Vector2 (speed, rb.velocity.y);
+			//transform.Translate (speed/10, 0, 0);
 		}
 
 		if(Input.GetKey("space")){
 			if(isGrounded){
+				isJumped = true;
 				rb.AddForce (transform.up * jumpForce * 100);
 			}
 		}
-			
 
 	}
 
-	void OnCollisionEnter(Collision collision){
-
-	}
 }
