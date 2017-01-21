615ab6220d9b46f8053bd77dbc641907ca3ba309
Algirdas <alzarkaitis@gmail.com>
2017-01-21 15:17:04 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/aasdas.unity

---
+++ Assets/Scripts/MapGen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGen : MonoBehaviour {
+	public GameObject Block;
+	public Texture2D heightmap;
+	public Vector3 size = new Vector3(100, 10, 100);
+	
+	void Update ()
+	{
+		int x = Mathf.FloorToInt (transform.position.x / size.x * heightmap.width);
+		int z = Mathf.FloorToInt (transform.position.z / size.z * heightmap.height);
+		Vector3 pos = transform.position;
+		pos.y = heightmap.GetPixel (x, z).grayscale * size.y;
+		Instantiate (Block, pos, Quaternion.identity);
+	}
+}
