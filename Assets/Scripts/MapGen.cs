cc2740ea5ef627f503980609fdb7c7341344e639
Algirdas <alzarkaitis@gmail.com>
2017-01-21 15:50:39 +0200

    Code

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -6,12 +6,23 @@ public class MapGen : MonoBehaviour {
 	public Texture2D heightmap;
 	public Vector3 size = new Vector3(100, 10, 100);
 	
-	void Update ()
-	{
+	void Load (){
+
+	
 		int x = Mathf.FloorToInt (transform.position.x / size.x * heightmap.width);
 		int z = Mathf.FloorToInt (transform.position.z / size.z * heightmap.height);
 		Vector3 pos = transform.position;
 		pos.y = heightmap.GetPixel (x, z).grayscale * size.y;
+
+
+		print(heightmap.GetPixel (0, 1).r);
+		print(heightmap.GetPixel (0, 1).g);
+		print(heightmap.GetPixel (0, 1).b);
 		Instantiate (Block, pos, Quaternion.identity);
 	}
-}
+
+	void Start(){
+		Load ();
+	}
+}		
+		
