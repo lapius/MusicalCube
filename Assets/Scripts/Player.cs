6826220be0b4912b207e569b2803922fea01c547
Marijus Lapinskas <marijus.lapinskas@gmail.com>
2017-01-21 02:21:50 +0200

    Player improve v0.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour {
 
 	public float speed;
 	public float jumpForce;
+	public float rotSpeed;
+
+
 	private Rigidbody2D rb;
 
 	public LayerMask ground;
@@ -15,6 +18,7 @@ public class Player : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		col = GetComponent<Collider2D> ();
+		rb.AddForce (transform.up * jumpForce * 100);
 	}
 	
 	// Update is called once per frame
@@ -22,16 +26,21 @@ public class Player : MonoBehaviour {
 
 		isGrounded = Physics2D.IsTouchingLayers (col, ground);
 
-		rb.AddForce (transform.right * speed);
+		rb.velocity = new Vector2 (speed, rb.velocity.y);
 		if (isGrounded) {
 			rb.velocity = new Vector2 (speed, 0);
+			this.gameObject.transform.Rotate(0, 0, 0);
+			transform.rotation = Quaternion.Euler(0,0,0);
+		} else if (!isGrounded) {
+			this.gameObject.transform.Rotate (0, 0, Time.deltaTime * rotSpeed);
 		}
 
 		if(Input.GetKey("space")){
 			if(isGrounded){
-				rb.AddForce (transform.up * jumpForce);
+				rb.AddForce (transform.up * jumpForce * 100);
 			}
 		}
+			
 
 	}
 
